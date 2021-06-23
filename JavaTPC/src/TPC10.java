af069a087ebec517e712eb8ce0477e3a81d8293c
임찬우 <@DESKTOP-DD6K95D.mshome.net>
2021-06-24 02:01:44 +0900

    2021-06-24 JAVA TPC
    기본자료형/사용자정의자료형(클래스)

---
+++ JavaTPC/src/TPC10.java
@@ -0,0 +1,10 @@
+import kr.tpc.BookDTO;
+public class TPC10 {
+
+	public static void main(String[] args) {
+		//책(BookDTO) 클래스(자료형:Datatype)를 만들자
+		BookDTO b;
+		
+	}
+
+}
---
+++ JavaTPC/src/kr/tpc/BookDTO.java
@@ -0,0 +1,14 @@
+
+public class BookDTO {
+	public String title;
+	public int price;
+	public String company;
+	public int page;
+
+	
+	//기본 생성자
+	public BookDTO() {
+		super();
+	}
+
+}
