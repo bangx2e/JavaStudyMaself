b1e9c284dae7c6d5f3c43e4567b908689fdc7149
임찬우 <@DESKTOP-DD6K95D.mshome.net>
2021-06-30 19:25:31 +0900

    git test

--- JavaTPC/src/TPC10.java
+++ JavaTPC/src/TPC10.java
@@ -1,9 +1,8 @@
-import kr.tpc.BookDTO;
 public class TPC10 {
 
 	public static void main(String[] args) {
 		//책(BookDTO) 클래스(자료형:Datatype)를 만들자
-		BookDTO b = new BookDTO;
+		BookDTO b = new BookDTO();
 		b.title = "자바책";
 		b.price= 17000;
 		b.company ="찬우";
